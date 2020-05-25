f8996538ad2b01b653ef3f267ec9042c9a65a2cf
Aqilah Misuary <aqilah.misuary@bandlab.com>
2020-05-25 21:34:15 +0200

    Chapter 3 Summary Ex 2

---
+++ ex2.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(void)
+{
+	double two_pi = 2.0 * M_PI; //in math.h
+	double h = 0.1;
+	double x;
+
+	for(x = 0.0; x < two_pi; x += h)
+	{	
+		printf("%5.1f:%.15f\n", x, sin(x) * sin(x) + cos(x) * cos(x));
+
+    }
+
+	return 0;
+}
