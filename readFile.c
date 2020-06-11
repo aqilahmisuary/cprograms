cbd93a6a5b8baec3c2cac75bf444f6b58124af12
Aqilah Misuary <aqilah.misuary@bandlab.com>
2020-06-11 17:49:54 +0200

    readFile

---
+++ readFile.c
@@ -0,0 +1,50 @@
+/*
+	Use of FILE I/O
+*/
+
+#include <stdio.h>
+
+#define MAX_HW 20
+
+void read_data(FILE *ptr, int d[], int *size)
+{
+	*size = 0;
+	while(fscanf(ptr, "%d",&d[*size]) == 1)
+		(*size)++;
+}
+
+void print_data(int d[], int size)
+{
+	int i;
+	for(i = 0; i < size; i++)
+	{
+		printf("%d\t", d[i]);
+		if ((i+ 1) % 10 == 0) printf("\n");
+	}
+}
+
+double average(int d[], int size)
+{
+	int i;
+	double avg = 0.0;
+	
+	for(i = 0; i < size; i++)
+		avg += d[i];
+	return(avg/size);	
+}
+
+int main()
+{
+	int i, sz = MAX_HW;
+	FILE *ifp;
+	int data[MAX_HW] = {100, 0};
+
+	ifp = fopen("myInt.txt", "r");
+	read_data(ifp, data, &sz);
+	printf("\nIntegers are: %d \n", sz);
+	print_data(data, sz);
+	printf("\nAverage score was %10f", average(data, sz));
+	printf("\n\n");
+	fclose(ifp);
+	return 0;
+}
